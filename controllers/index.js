80a9656d0afb243262ddcd83863367edec855467
jvhti <jvhti@hotmail.com>
2020-10-26 00:31:33 -0300

    Add front-end production build

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,11 @@
 'use strict';
 
-var IndexModel = require('../models/index');
-
-
 module.exports = function (router) {
 
-    var model = new IndexModel();
-
     router.get('/', function (req, res) {
-        
-        res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
-        
+
+        res.redirect('index.html');
+
     });
 
 };
